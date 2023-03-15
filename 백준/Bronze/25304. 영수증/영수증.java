807098141db175841f63697b77234982065c0acb
leemyunga <104979981+leemyunga@users.noreply.github.com>
2023-03-15 14:51:44 +0900

    [Bronze V] Title: 영수증, Time: 224 ms, Memory: 17896 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int X = sc.nextInt();
+		int N = sc.nextInt();
+		int sum = 0;
+		
+		for (int i = 0; i < N; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();	
+			sum += a*b;
+			//System.out.println(a + " / " +b + " / " + sum);
+		}
+		
+		if(sum==X) {
+			System.out.println("Yes");
+		}else {
+			System.out.println("No");
+		}
+		
+		sc.close();
+	}
+
+}
