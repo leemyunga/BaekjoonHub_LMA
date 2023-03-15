c61496b4e8e0118a42f5d2acc032130304e09746
leemyunga <104979981+leemyunga@users.noreply.github.com>
2023-03-15 17:20:17 +0900

    [Bronze V] Title: A+B - 8, Time: 180 ms, Memory: 16448 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,29 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < T; i++) {
+			String st = br.readLine();
+			int A = Integer.parseInt(st.split(" ")[0]);
+			int B = Integer.parseInt(st.split(" ")[1]);
+			bw.write("Case #"+(i+1)+": "+A+" + "+B+" = "+(A+B)+"\n");
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
