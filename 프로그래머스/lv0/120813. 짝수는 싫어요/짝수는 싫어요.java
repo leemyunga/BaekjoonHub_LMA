2e7fc8d03f62588f8c3cde950bc50441552d7475
LEE Myung-A <104979981+leemyunga@users.noreply.github.com>
2023-07-31 13:15:58 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int n) {
+        int size = (n%2)==0 ? n/2 : n/2+1;
+        int[] answer = new int[size];
+        int k = 0;
+        for(int i=1; i<=n; i++){
+            if(i%2 == 1){
+                answer[k]=i;
+                k++;
+            }
+        }
+        return answer;
+    }
+}
