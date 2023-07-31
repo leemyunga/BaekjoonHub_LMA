1efc322b8752ae6c15d64951d6e9074cb6f99626
LEE Myung-A <104979981+leemyunga@users.noreply.github.com>
2023-07-31 11:18:55 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+    	int answer = 0;
+    	String str = String.valueOf(n);
+    	char[] ch = str.toCharArray();
+    	for (char c : ch) {
+			answer += (int)(c-'0'); 
+		}
+        return answer;
+    }
+}
