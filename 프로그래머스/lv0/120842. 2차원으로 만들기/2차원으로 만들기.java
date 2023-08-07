da754182d7d1c3bed1c14f2c11b40325e8bc974b
LEE Myung-A <104979981+leemyunga@users.noreply.github.com>
2023-08-07 15:18:49 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[][] solution(int[] num_list, int n) {
+        int[][] answer = new int[num_list.length / n][n];
+        int cnt = 0;
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < answer[i].length; j++, cnt++) {
+                answer[i][j] = num_list[cnt];
+            }
+        }
+        return answer;
+    }
+}
