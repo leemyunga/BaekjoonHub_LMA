b79939a778cd22b10e2b77b4ee1ad3fddb824c31
LEE Myung-A <104979981+leemyunga@users.noreply.github.com>
2023-08-11 02:29:57 +0900

    [level 0] Title: 소인수분해, Time: 0.37 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+        int[] answer;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int index = 0;
+        
+        for(int i = 2; i <= n;)
+        {
+        	if(n%i==0) 
+        	{
+        		map.put(i, i);
+        		n/=i; 
+        	}
+        	else 
+        	{
+        		i++;
+        	}
+        	
+        }
+        
+        answer = new int[map.size()];
+        for(Integer i : map.keySet())
+        {
+        	answer[index] = map.get(i);
+        	index++;
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
