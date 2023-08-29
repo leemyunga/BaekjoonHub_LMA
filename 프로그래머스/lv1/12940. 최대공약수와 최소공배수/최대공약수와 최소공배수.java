6f17a206fc81a504afcfdad4ee06fc3f64f84a57
LEE Myung-A <104979981+leemyunga@users.noreply.github.com>
2023-08-29 23:08:11 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.05 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int n, int m) {
+
+        int max = 1;
+
+        for(int i=1; i <=n && i <= m; i++){
+            if((n%i == 0) && (m%i == 0)){
+                max= i;
+            }
+        }
+
+        int min = (n*m)/max;
+
+        int[] answer = {max,min};
+        return answer;
+    }
+}
